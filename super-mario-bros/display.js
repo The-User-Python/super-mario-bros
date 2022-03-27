c8a13d3a20bfcabc37d00169de20b59b8507de03
Daksh <agmdaksh@gmail.com>
2022-03-27 11:06:07 -0700

    Update display.js

--- super-mario-bros/display.js
+++ super-mario-bros/display.js
@@ -4,8 +4,8 @@
    *
    * Backbone Game Engine - An elementary HTML5 canvas game engine using Backbone.
    *
-   * Copyright (c) 2014 Martin Drapeau
-   * https://github.com/martindrapeau/backbone-game-engine
+   * Copyright (c) 2014 Daksh Hande
+   * https://github.com/dakshcodess/backbone-game-engine
    *
    */
    
